cde50cca24fceab04af4f3dd57467fd1ad28d0a2
David <wjw3298@gmail.com>
2024-07-24 16:52:31 -0400

    added color/ box, need data for words

--- playWordle.py
+++ playWordle.py
@@ -1,20 +1,20 @@
 from wordle import Wordle
 from colorama import Fore
+from letterState import LetterState
 
 def main():
     print ("Hello Wordle")
     wordle = Wordle('Apple')
 
     while wordle.canAttempt:
-        x = input("Type yout guess: ")
+        x = input("\n Type yout guess: ")
 
         if len(x) != wordle.WORD_LENGTH:
             print(Fore.RED + f'Word must be {wordle.WORD_LENGTH} characters long' + Fore.RESET)
             continue
 
         wordle.attempt(x)
-        result = wordle.guess(x)
-        print(*result , sep='\n')
+        displayResults(wordle)
         
     if wordle.isSolved:
         print('Puzzle solved')
@@ -23,6 +23,47 @@ def main():
 
     print (wordle)
 
+def displayResults(wordle: Wordle):
+    print('\nResult so far...')
+    print(f'You have {wordle.remainingAttempts} attempts left\n')
+
+    lines = []
+
+    for word in wordle.attempts:
+        result = wordle.guess(word)
+        coloredResultStr = convertResultToColor(result)
+        lines.append(coloredResultStr)
+    
+    for _ in range(wordle.remainingAttempts):
+        lines.append(" ".join('_' * wordle.WORD_LENGTH))
+
+    drawBorder(lines)
+
+def convertResultToColor(result: list[LetterState]):
+    resultWithColor = []
+    for letter in result:
+        if letter.isInPosition:
+            color = Fore.GREEN
+        elif letter.isInWord:
+            color = Fore.YELLOW
+        else:
+            color = Fore.WHITE
+        coloredLetter = color + letter.character + Fore.RESET
+        resultWithColor.append(coloredLetter)
+    return " ".join(resultWithColor)
+
+def drawBorder(lines:list[str], size: int = 9, pad: int = 1):
+
+    contentLength = size + pad * 2
+    topBorder = '┌' + '─' * contentLength + '┐'
+    bottomBorder = '└' + '─' * contentLength + '┘'
+    space = " " * pad
+    print(topBorder)
+
+    for line in lines:
+        print("│" + space + line + space + "│")
+
+    print(bottomBorder)
 
 
 
